784486a4273d095cd857e0d3998fd088e72eb287
Negin Bandar <bandar.negin@gmail.com>
2024-10-27 14:29:05 -0400

    cleanup

--- routes/categories-route.js
+++ routes/categories-route.js
@@ -1,5 +1,4 @@
 import express from "express";
-//import { v4 as uuidv4 } from "uuid";
 import fs from "fs";
 
 const router = express.Router();
--- routes/users-route.js
+++ routes/users-route.js
@@ -1,5 +1,4 @@
 import express from "express";
-//import { v4 as uuidv4 } from "uuid";
 import fs from "fs";
 
 const router = express.Router();
@@ -7,18 +6,6 @@ const router = express.Router();
 router.route("/").get((req, res) => {
   try {
     const users = JSON.parse(fs.readFileSync("./data/users.json"));
-    // const user = users.map((user) => {
-    //   return {
-    //     user_id: user.user_id,
-    //     user_type: user.user_type,
-    //     username: user.username,
-    //     password: user.password,
-    //     first_name: user.first_name,
-    //     last_name: user.last_name,
-    //     profile_picture: user.profile_picture,
-    //     profile_about: user.profile_about,
-    //   };
-    // });
     res.json(users);
   } catch (error) {
     console.log(error);
